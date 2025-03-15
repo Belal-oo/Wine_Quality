ebd237ff61e1dedf2506e7ee32def3882520d1a6
Belal-oo <belamohamed386@gmail.com>
2025-03-16 00:45:39 +0200

    Commit_Frontend

--- server.py
+++ server.py
@@ -1,44 +1,46 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify ,render_template
 import joblib    
 import pandas as pd
+import numpy as np 
+
+app = Flask(__name__)
 
 with open("wine_quality_model.joblib", "rb") as f:
     model = joblib.load(f)
 
-app = Flask(__name__)
-
-
 @app.route('/')
 def home():
-    return "Run Successfully......"
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
-        json_data = request.get_json()
-    
-        data = pd.DataFrame(json_data)
-    
-        data.columns = [col.replace("_", " ") for col in data.columns]
+        int_features = [float(x) for x in request.form.values()]
 
+        final_features = [np.array(int_features)]
+        
+        data = pd.DataFrame(final_features)
+    
     
         expected_columns = ['fixed acidity', 'volatile acidity', 'citric acid', 
                             'residual sugar', 'chlorides', 'free sulfur dioxide', 
                             'total sulfur dioxide', 'density', 'pH', 'sulphates', 'alcohol']
-        data = data[expected_columns]
-    
-        # إجراء التنبؤ باستخدام النموذج
+        
+        
+        if len(data.columns) == len(expected_columns):
+            data.columns = expected_columns
+        else:
+            raise ValueError("Number of input features does not match expected features.")
+        
+        
         prediction = model.predict(data)
     
         result = "Good Wine" if prediction[0] == 1 else "Bad Wine"
     
-        return jsonify({'Prediction': result})
-    
+        return render_template('index.html', prediction_text='Prediction {}'.format(result))
     except Exception as e:
-        return jsonify({"error": str(e)})
+        app.logger.error(f"Error during prediction: {str(e)}")
+        return render_template('index.html', prediction_text='Prediction: {}'.format(result))
 
-
-
-if __name__== '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",debug=True,port=8888)
